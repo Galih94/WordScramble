79af1be1e39d09042299a41c29fb2ede41a435e7
galih <samudragalih94@gmail.com>
2024-05-14 10:46:05 +0700

    feat(UI): uset `Section` inside `List`

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -10,11 +10,29 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         List {
-            Text("Header")
-            ForEach(0..<5) {
-                Text("Hello, world! \($0)")
+            Section("Section 1") {
+                Text("Header")
+                ForEach(0..<5) {
+                    Text("Hello, world! \($0)")
+                }
+                Text("Footer")
             }
-            Text("Footer")
+            
+            Section("Section 2") {
+                Text("Header")
+                ForEach(0..<2) {
+                    Text("Hello, world! \($0)")
+                }
+                Text("Footer")
+            }
+            
+            Section("Section 3") {
+                Text("Header")
+                ForEach(0..<1) {
+                    Text("Hello, world! \($0)")
+                }
+                Text("Footer")
+            }
         }
     }
 }
