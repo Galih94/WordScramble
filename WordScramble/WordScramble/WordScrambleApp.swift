e38ea9a08e6a9ab7d491d874f3b24857c98b0bc4
galih <samudragalih94@gmail.com>
2024-05-14 10:12:08 +0700

    wip: initial push project `WordScramble`

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Galih Samudra on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
