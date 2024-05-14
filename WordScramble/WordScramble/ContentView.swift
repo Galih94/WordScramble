bd8b476f1906ddda07d47816f19d050bfb674f96
galih <samudragalih94@gmail.com>
2024-05-14 10:50:29 +0700

    feat(UI): make `List` become purely dynamic content

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,25 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        List {
-            Section("Section 1") {
-                Text("Header")
-                ForEach(0..<5) {
-                    Text("Hello, world! \($0)")
-                }
-                Text("Footer")
-            }
-            
-            Section("Section 2") {
-                ForEach(0..<2) {
-                    Text("Hello, world! \($0)")
-                }
-            }
-            
-            Section("Section 3") {
-                Text("Header")
-                Text("Footer")
-            }
+        List(0..<5) {
+            Text("Hello, world! \($0)")
         }
         .listStyle(.grouped)
     }
